60b0cd8345828a5ab64cd445f66a633acf8b8d5d
francosta <francisco@fcosta.pt>
2019-07-16 10:24:34 +0100

    Added chalk npm moduke.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const chalk = require("chalk");
+const log = console.log;
+// Node does not support ES6 at the moment - This wouldn't work: import validator from "validator";
 const getNotes = require("./notes");
 
 console.log(getNotes());
+const msg = chalk.green.bold.inverse("Success!");
+
+log(msg);
