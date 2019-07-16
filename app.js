5a38bfae089af31c89b5847e58c7950746801d10
francosta <francisco@fcosta.pt>
2019-07-16 12:00:18 +0100

    Installed yargs

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const log = console.log;
 // Node does not support ES6 at the moment - This wouldn't work: import validator from "validator";
 const getNotes = require("./notes");
 
-console.log(getNotes());
-const msg = chalk.green.bold.inverse("Success!");
+// This is the command that the user will input: the first word after the filename in the run command.
+const command = process.argv[2];
 
-log(msg);
+// We can access commands from the terminal with process.
+// Process gives us an array. The first and second will be paths. One to the node.js executable in the machine and the second to the file that has been executed.
+// The third is a string from the argument.
+
+if (command === "add") {
+  log("Adding a note!");
+} else if (command === "remove") {
+  log("Removing a note!");
+}
